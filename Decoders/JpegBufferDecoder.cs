46d1353fc5125e0bad1fb64e901cfa1d4eb8d329
Marcos Cordeiro <marcos.cordeiro@andbank.com.br>
2020-11-05 17:57:50 -0300

    Extract SignatureBasedDecoder from Jpeg Decoder
    Also fix for when buffer does not form an image.

--- Decoders/JpegBufferDecoder.cs
+++ Decoders/JpegBufferDecoder.cs
@@ -1,143 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Channels;
-using System.Threading.Tasks;
+﻿using System.Threading.Channels;
 
 namespace FrameExtractor.Decoders
 {
-    internal class JpegBufferDecoder : IFrameBufferDecoder
+    internal class JpegBufferDecoder : SignatureBasedDecoder
     {
-        public JpegBufferDecoder(ChannelWriter<Frame> channelWriter)
-        {
-            ChannelWriter = channelWriter;
-        }
-
-        private int CurrentFrame { get; set; }
-        private List<byte> LastBuffer { get; } = new List<byte>();
         private static byte[] StartSignature { get; } = {255, 216, 255};
         private static byte[] EndSignature { get; } = {255, 217};
 
-        public ChannelWriter<Frame> ChannelWriter { get; }
-
-        private bool FrameStarted { get; set; }
-
-        private int? PositionStart { get; set; }
-        private int? PositionEnd { get; set; }
-
-        public async Task WriteAsync(byte[] buffer)
+        public JpegBufferDecoder(ChannelWriter<Frame> channelWriter) : base(StartSignature, EndSignature, channelWriter)
         {
-            var currentStartSignaturePosition = 0;
-            var currentEndSignaturePosition = 0;
-
-
-            for (var index = 0; index < buffer.Length; index++)
-            {
-                var b = buffer[index];
-
-                if (!FrameStarted && b == StartSignature[currentStartSignaturePosition])
-                {
-                    var length = StartSignature.Length - 1;
-                    if (currentStartSignaturePosition == length)
-                    {
-                        FrameStarted = true;
-                        PositionStart = index - length;
-                        currentStartSignaturePosition = 0;
-                    }
-                    else
-                    {
-                        currentStartSignaturePosition++;
-                    }
-                }
-                else
-                {
-                    currentStartSignaturePosition = 0;
-                }
-
-                if (FrameStarted && b == EndSignature[currentEndSignaturePosition])
-                {
-                    var length = EndSignature.Length - 1;
-                    if (currentEndSignaturePosition == length)
-                    {
-                        PositionEnd = index + 1;
-
-                        if (PositionStart.HasValue)
-                        {
-                            var bytes = buffer[PositionStart.Value..PositionEnd.Value];
-                            await CreateImage(bytes);
-                            PositionStart = null;
-                        }
-                        else
-                        {
-                            if (LastBuffer.Count > 0)
-                            {
-                                var array = LastBuffer.Concat(buffer[..PositionEnd.Value]).ToArray();
-                                await CreateImage(array);
-                                LastBuffer.Clear();
-                            }
-                        }
-
-                        FrameStarted = false;
-                        currentEndSignaturePosition = 0;
-                    }
-                    else
-                    {
-                        currentEndSignaturePosition++;
-                    }
-                }
-                else
-                {
-                    currentEndSignaturePosition = 0;
-                }
-            }
-
-            if (PositionEnd.HasValue)
-            {
-                var delta = buffer.Length - PositionEnd.Value;
-
-                if (delta > 1)
-                {
-                    LastBuffer.AddRange(buffer[PositionEnd.Value..]);
-                }
-            }
-
-            PositionStart = null;
-            PositionEnd = null;
-        }
-
-        private async Task CreateImage(byte[] data)
-        {
-#if DEBUG
-            var malformed = false;
-
-            for (var index = 0; index < StartSignature.Length; index++)
-            {
-                if (StartSignature[index] != data[index])
-                {
-                    malformed = true;
-                }
-            }
-
-            var dataIndex = 0;
-            for (var index = EndSignature.Length - 1; index >= 0; index--)
-            {
-                if (EndSignature[index] != data[data.Length - 1 - dataIndex])
-                {
-                    malformed = true;
-                }
-
-                dataIndex++;
-            }
-
-            if (malformed)
-            {
-                Console.WriteLine("Malformed frame.");
-                throw new InvalidOperationException("Malformed frame.");
-            }
-#endif
-
-            await ChannelWriter.WriteAsync(new Frame(data, CurrentFrame));
-            CurrentFrame++;
         }
     }
 }
---
+++ Decoders/SignatureBasedDecoder.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace FrameExtractor.Decoders
+{
+    internal class SignatureBasedDecoder : IFrameBufferDecoder
+    {
+        protected SignatureBasedDecoder(byte[] startSignature, byte[] endSignature, ChannelWriter<Frame> channelWriter)
+        {
+            StartSignature = startSignature;
+            EndSignature = endSignature;
+            ChannelWriter = channelWriter;
+        }
+
+        private int CurrentFrame { get; set; }
+        private List<byte> LastBuffer { get; } = new List<byte>();
+        private byte[] StartSignature { get; }
+        private byte[] EndSignature { get; }
+        private bool FrameStarted { get; set; }
+        private int? PositionStart { get; set; }
+        private int? PositionEnd { get; set; }
+        public ChannelWriter<Frame> ChannelWriter { get; }
+
+        public virtual async Task WriteAsync(byte[] buffer)
+        {
+            var currentStartSignaturePosition = 0;
+            var currentEndSignaturePosition = 0;
+            var imageCreated = false;
+
+
+            for (var index = 0; index < buffer.Length; index++)
+            {
+                var b = buffer[index];
+
+                if (!FrameStarted && b == StartSignature[currentStartSignaturePosition])
+                {
+                    var length = StartSignature.Length - 1;
+                    if (currentStartSignaturePosition == length)
+                    {
+                        FrameStarted = true;
+                        PositionStart = index - length;
+                        currentStartSignaturePosition = 0;
+                    }
+                    else
+                    {
+                        currentStartSignaturePosition++;
+                    }
+                }
+                else
+                {
+                    currentStartSignaturePosition = 0;
+                }
+
+                if (FrameStarted && b == EndSignature[currentEndSignaturePosition])
+                {
+                    var length = EndSignature.Length - 1;
+                    if (currentEndSignaturePosition == length)
+                    {
+                        PositionEnd = index + 1;
+
+                        if (PositionStart.HasValue)
+                        {
+                            var bytes = buffer[PositionStart.Value..PositionEnd.Value];
+                            await CreateImage(bytes);
+                            imageCreated = true;
+                            PositionStart = null;
+                        }
+                        else
+                        {
+                            if (LastBuffer.Count > 0)
+                            {
+                                var array = LastBuffer.Concat(buffer[..PositionEnd.Value]).ToArray();
+                                await CreateImage(array);
+                                imageCreated = true;
+                                LastBuffer.Clear();
+                            }
+                        }
+
+                        FrameStarted = false;
+                        currentEndSignaturePosition = 0;
+                    }
+                    else
+                    {
+                        currentEndSignaturePosition++;
+                    }
+                }
+                else
+                {
+                    currentEndSignaturePosition = 0;
+                }
+            }
+
+            if (PositionEnd.HasValue)
+            {
+                var delta = buffer.Length - PositionEnd.Value;
+
+                if (delta > 1) LastBuffer.AddRange(buffer[PositionEnd.Value..]);
+            } else if (PositionEnd == null && !imageCreated)
+            {
+                LastBuffer.AddRange(buffer);
+            }
+
+            PositionStart = null;
+            PositionEnd = null;
+        }
+
+        private async Task CreateImage(byte[] data)
+        {
+#if DEBUG
+            var malformed = false;
+
+            for (var index = 0; index < StartSignature.Length; index++)
+                if (StartSignature[index] != data[index])
+                    malformed = true;
+
+            var dataIndex = 0;
+            for (var index = EndSignature.Length - 1; index >= 0; index--)
+            {
+                if (EndSignature[index] != data[data.Length - 1 - dataIndex]) malformed = true;
+
+                dataIndex++;
+            }
+
+            if (malformed)
+            {
+                Console.WriteLine("Malformed frame.");
+                //throw new InvalidOperationException("Malformed frame.");
+            }
+#endif
+
+            await ChannelWriter.WriteAsync(new Frame(data, CurrentFrame));
+            CurrentFrame++;
+        }
+    }
+}
